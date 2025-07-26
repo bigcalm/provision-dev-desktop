eb51230a241a3bc05abf02be8149bcfaa2415647
Iain Cuthbertson <bigcuthy@gmail.com>
2025-07-26 14:36:48 +0100

    Adds fw16 host and install ansible roles before provisioning

--- provision.sh
+++ provision.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+ansible-galaxy install -f -r ansible/requirements.yml -p ansible/roles
+
 ansible-playbook -i ansible/inventories/$1.yml ansible/provision.yml --ask-become-pass
